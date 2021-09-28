6a79adf7558defbd82711da4db9475389d3641de
@mrit Bhatt <61137260+amritevon@users.noreply.github.com>
2021-09-28 18:45:28 +0530

    config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.SERVER_PORT || 5100;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log('main app listening on port => ', port);
 }
 bootstrap();
