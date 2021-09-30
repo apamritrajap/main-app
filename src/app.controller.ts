65d7622dba748269a932e0bf2484fa70bde17578
@mrit Bhatt <61137260+amritevon@users.noreply.github.com>
2021-09-30 10:16:26 +0530

    aaded rabbitmq, communication between services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {
-    console.log('in app controller main app');
+  constructor(
+    private readonly appService: AppService,
+    @Inject('GOLEGAL_SERVICE') private readonly client: ClientProxy,
+  ) {}
+
+  async onApplicationBootstrap() {
+    try {
+      await this.client.connect();
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get()
-  getHello(): string {
+  getHello() {
+    const data = {
+      msg: 'please recieve this and show in console',
+      test: 'testing',
+    };
+    // const test = this.client.emit('helloBus', heheh);
+    const test = this.client.send('eventBus', data);
+    test.subscribe((incomingData) => {
+      console.log('---------->', incomingData);
+    });
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ClientsModule.register([
+      {
+        name: 'GOLEGAL_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          // urls: ['amqp://admin:admin@localhost:5672'],
+          urls: [process.env.RABBIT_URL],
+          queue: process.env.RABBIT_QUEUE,
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
